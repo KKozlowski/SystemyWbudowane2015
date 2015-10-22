2761102a089fe0b5bfc727004d79d43087b3e117
Ringgg <ringlan@gmail.com>
2015-10-22 20:26:46 +0200

    -audio Update

--- Arduino/sketch_oct16a/Audio.h
+++ Arduino/sketch_oct16a/Audio.h
@@ -1,15 +1,35 @@
 #include "VGAX.h"
 
-#define delay 10;
+#define delay 50;
 #define size 12;
 class Audio
 {
   private:
-  static int beats[]  = { 16, 16, 16,  8,  8,  16, 32, 16, 16, 16, 8, 8 }; 
+  static int beats[size]  = { 16, 16, 16,  8,  8,  16, 32, 16, 16, 16, 8, 8 }; 
   static byte counter = 0;
   static byte index = 0;
 
   public:
+
+  static void Init()
+  {
+    counter = 0;
+    index = 0;
+
+    beats[0] = 16;
+    beats[1] = 16;
+    beats[2] = 16;
+    beats[3] = 8;
+    beats[4] = 8;
+    beats[5] = 16;
+    beats[6] = 32;
+    beats[7] = 16;
+    beats[8] = 16;
+    beats[9] = 16;
+    beats[10] = 8;
+    beats[11] = 8;
+  }
+  
   static void Update()
   {
     ++counter %= delay;
@@ -20,5 +40,5 @@ class Audio
       ++index %= size;
     }
   }
-}
+};
 
