4d65089b2de1a509a10a18ed88632fe950b0fa51
kkozlowski <septuater@gmail.com>
2015-10-22 20:01:08 +0200

    Added player's death, caused by collision.

--- Arduino/sketch_oct16a/Game.h
+++ Arduino/sketch_oct16a/Game.h
@@ -32,7 +32,7 @@ class Game
       if(obs[i].enabled)
       {
         obs[i].Draw();
-        if (obs[i].posY >= 50)
+        if (obs[i].posY >= 64)
         {
           //checkCollision
           obs[i].enabled = false;
@@ -43,6 +43,17 @@ class Game
     ++spawnTimer;
     if(!spawnTimer)
       SpawnObstacle();
+
+    for (int i = 0; i < obstCount; ++i)
+    {
+      if(obs[i].posY > 52 && obs[i].posY <56){
+        if (rocket.CollidesWith(obs[i].posX))
+          rocket.Die();
+        }
+      }
+    
+    
+    //rocket.Die();
   }
 
   void SpawnObstacle()
--- Arduino/sketch_oct16a/Rocket.h
+++ Arduino/sketch_oct16a/Rocket.h
@@ -5,9 +5,10 @@ class Rocket
   int temp=0;
   int position;
   short positionY = 52;
-  
+  bool alive = true;
 
   public:
+  
   void Clear()//żeby nie VGAX::clear(0) bo czyści cały ekran i kurwa mruga, maluje tam gdzie była rakieta czarny po czym maluje rakiete
   {
     VGAX::fillrect(temp-1, 52, 3, 6, 0);
@@ -17,26 +18,40 @@ class Rocket
     VGAX::fillrect(temp+3, 55, 1, 4, 0);
     VGAX::fillrect(temp+2, 56, 1, 3, 0);//czerwone
   }
+
+  void Die(){
+    alive = false;
+    /*while(true){
+       VGAX::fillrect(0, 0, 120, 60, 1);
+    }*/
+  }
+
+  bool IsAlive(){
+    return alive;
+  }
+  
   void Draw ()
   {
-    position = (analogRead(A2)/9.7f)+7;
-    if(temp!=position)
-    {
-    //VGAX::clear(0);  
-    Clear();
-    //cialo
-    VGAX::fillrect(position-1, 52, 3, 6, 3);
-    //lotki
-    VGAX::fillrect(position-3, 55, 1, 4, 3);
-    VGAX::fillrect(position-2, 56, 1, 3, 1);//czerwone
-    VGAX::fillrect(position+3, 55, 1, 4, 3);
-    VGAX::fillrect(position+2, 56, 1, 3, 1);//czerwone
-    temp=position;
+    if(alive){
+      position = (analogRead(A2)/9.7f)+7;
+      if(temp!=position)
+      {
+      //VGAX::clear(0);  
+      Clear();
+      //cialo
+      VGAX::fillrect(position-1, 52, 3, 6, 3);
+      //lotki
+      VGAX::fillrect(position-3, 55, 1, 4, 3);
+      VGAX::fillrect(position-2, 56, 1, 3, 1);//czerwone
+      VGAX::fillrect(position+3, 55, 1, 4, 3);
+      VGAX::fillrect(position+2, 56, 1, 3, 1);//czerwone
+      temp=position;
+      }
     }
   }
 
   bool CollidesWith(int obstaclePos)
   {
-    return position >= obstaclePos - 1 && position <= obstaclePos + 1;
+    return position >= obstaclePos - 2 && position <= obstaclePos + 2;
   }
 };
